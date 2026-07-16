e7648bd20d9abdf616cc31897977faef7ce0e067
Azer-khadhraoui <azerronaldo2004@gmail.com>
2026-07-16 10:17:33 +0100

    feat(auth): implement signup component with form validation and routing

--- frontend/src/app/auth/login/login.component.ts
+++ frontend/src/app/auth/login/login.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, RouterLink],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
@@ -12,7 +13,7 @@ export class LoginComponent {
   private readonly fb = new FormBuilder();
 
   loginForm = this.fb.group({
-    email: ['', [Validators.required, Validators.email]],
+    matricule: ['', [Validators.required, Validators.pattern(/^\d{6}$/)]],
     password: ['', [Validators.required, Validators.minLength(6)]]
   });
 
